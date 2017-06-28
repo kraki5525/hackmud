bc92cab742b23bfc2bdecec7f88dc0ff63dc6ab0
Richard McCollister <kraki5525@gmail.com>
2017-06-28 00:14:33 -0400

    working on method to iterate over users

--- findUsers/script.js
+++ findUsers/script.js
@@ -1,17 +1,25 @@
-function (c, arg) { 
-    function is_npc(name) {
-        var last_action = #s.users.last_action({ name: [name, 'gibson'] });
-        return last_action[0] && last_action[0].t.getTime() == last_action[1].t.getTime();
-    }
+function (c, arg) {
+	function is_npc(name) {
+		var last_action = #s.users.last_action({ name: [name, 'gibson'] });
+		return last_action[0] && last_action[0].t.getTime() == last_action[1].t.getTime();
+	}
 
-    var l = #s.scripts.lib(),	
-        scripts = #s.scripts.fullsec(),
-        usrs = scripts.map(x => x.split('.')[0])
-            .filter(x => { 
-                return is_npc(x);
-            });
-	
-    return { ok:true, msg: usrs	};
+	var scripts = [],
+		ind = 0,
+		t = #s.scripts.fullsec({ start: ind });
+	while (t.length > 0) {
+		scripts = scripts.concat(t);
+		ind = ind + 128;
+		t = #s.scripts.fullsec({ start: ind });
+	}
+
+	// var usrs = scripts.map(x => x.split('.')[0])
+	// 	.filter(x => {
+	// 		return is_npc(x);
+	// 	});
+
+	return {
+		ok: true,
+		msg: scripts
+	};
 };
-
-
