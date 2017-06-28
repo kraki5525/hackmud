f5c6ca2b7aa6de4b001e5528b0447da0fb64f4f0
Richard McCollister <kraki5525@gmail.com>
2017-06-27 23:46:48 -0400

    cleanup findUser script

--- findUsers/script.js
+++ findUsers/script.js
@@ -1,19 +1,17 @@
 function (c, arg) { 
-	function is_npc(name) {
-    	var last_action = #s.users.last_action({ name: [name, "gibson"] });
-    	return last_action[0] && last_action[0].t.getTime() == last_action[1].t.getTime();
-    	//return last_action;
-	}
+    function is_npc(name) {
+        var last_action = #s.users.last_action({ name: [name, 'gibson'] });
+        return last_action[0] && last_action[0].t.getTime() == last_action[1].t.getTime();
+    }
 
-	var l = #s.scripts.lib(),	
-		scripts = #s.scripts.fullsec(),
-		usrs = scripts.map(x => x.split('.')[0])
-				.filter(x => { 
-					return is_npc(x);
-					//return {name: x, npc: is_npc(x)};
-				});
+    var l = #s.scripts.lib(),	
+        scripts = #s.scripts.fullsec(),
+        usrs = scripts.map(x => x.split('.')[0])
+            .filter(x => { 
+                return is_npc(x);
+            });
 	
-	return { ok:true, msg: usrs	};
-}
+    return { ok:true, msg: usrs	};
+};
 
 
