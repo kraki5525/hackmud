a1a63cd164423915e667ef2cf3616d0b3437a63e
Rich McCollister <kraki5525@gmail.com>
2017-07-05 23:54:40 -0400

    Adding c002 and c003, rework to reduce code count

--- lockpick/script.js
+++ lockpick/script.js
@@ -1,32 +1,42 @@
 function (c, a) {
     var names = ['open', 'unlock', 'release'],
         colors = ['red', 'blue', 'green', 'orange', 'yellow', 'cyan', 'purple', 'lime'],
-        primes = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
-            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
-        ],
+        primes = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97],
         locked = /correct/,
         ez21 = 'EZ_21',
         ez35 = 'EZ_35',
         ez40 = 'EZ_40',
         c001 = 'c001',
+        c002 = 'c002',
+        c003 = 'c003',
         arg = {},
-        _ = (array, func) => array.forEach(func),
-        result = a.t.call(arg);
+        _n = (array, type) =>{
+            array.find(x => {
+                arg[type] = x
+                result = a.t.call(arg)
+                if (!locked.test(result)) {
+                    return true;
+                }
+            })
+            throw 0;
+        },
+        result = a.t.call(arg),
+        locks = {
+            [ez21]: ez_21,
+            [ez35]: ez_35,
+            [ez40]: ez_40,
+            [c001]: c_001,
+            [c002]: c_002,
+            [c003]: c_003,
+        };
 
     try {
         while (/LOCK_ERROR/.test(result)) {
-            if (result.indexOf(ez21)>0) {
-                arg = ez_21(arg);
-            } else if (result.indexOf(ez35) > 0) {
-                arg = ez_35(arg);
-            } else if (result.indexOf(ez40) > 0) {
-                arg = ez_40(arg);
-            } else if (result.indexOf(c001) > 0) {
-                arg = c_001(arg);
-            }            
-            else {
+            var lock = Object.keys(locks).find(lock => result.indexOf(lock) > 0);
+            if (lock)
+                locks[lock]();
+            else
                 throw 0;
-            }
         }
     }
     catch (e) {
@@ -35,68 +45,35 @@ function (c, a) {
 
     return {isOk: true,msg: result};
 
-    function c_001(args) {
-        _(colors, color => {
-            args[c001] = color
-            result = a.t.call(args)
-            if (!locked.test(result)) {
-                return;
-            }
-        });
-
-        args.color_digit = args[c001].length;
-        result = a.t.call(args);
-        return args;    
-    }
-    function ez_21(args) {
-        _(names, name => {
-            args[ez21] = name
-            result = a.t.call(args)
-            if (!locked.test(result)) {
-                return args;
-            }
-        })
-
-        throw 0;
+    function c_001() {
+        _n(colors, c001);
+        arg.color_digit = arg[c001].length;
+        result = a.t.call(arg);
+        return;    
     }
 
-    function ez_35(args) {
-        _(names, name => {
-            args[ez35] = name
-            result = a.t.call(args)
-            if (!locked.test(result)) {
-                return;
-            }
-        });
-
-        for (var i=0; i < 10; i++) {
-            args.digit = i;
-            result = a.t.call(args)
-            
-            if (!locked.test(result)) {
-                return args;
-            }
-        }
-        throw 0;
+    function c_002() {
+        _n(colors, c002);
+        _n(colors, 'c002_complement');
     }
 
-    function ez_40(args) {
-        _(names, name => {
-            args[ez40] = name
-            result = a.t.call(args)
-            if (!locked.test(result)) {
-                return;
-            }
-        });
+    function c_003() {
+        _n(colors, c003);
+        _n(colors, 'c003_triad_1');
+        _n(colors, 'c003_triad_2');
+    }
 
-        _(primes, prime => {
-            args.ez_prime = prime
-            result = a.t.call(args)
-            if (!locked.test(result)) {
-                return args;
-            }
-        });
+    function ez_21() {
+        _n(names, ez21);
+    }
 
-        throw 0;
+    function ez_35() {
+        _n(names, ez35);
+        _n([...Array(5).keys()], "digit");
+    }
+
+    function ez_40() {
+        _n(names, ez40);
+        _n(primes, "ez_prime");
     }
 }
