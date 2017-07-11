bd0e8ff2833c811581aaa9130a3654e409493fe3
Richard McCollister <kraki5525@gmail.com>
2017-07-10 23:48:37 -0400

    rework findUsers to allow searching all levels

--- findUsers/script.js
+++ findUsers/script.js
@@ -1,4 +1,4 @@
-function (c, arg) { //start:0, pages:1
+function (c, arg) { //start:0, pages:1, level:4
     function is_npc(name) {
         var last_action = #s.users.last_action({
             name: [name, 'gibson']
@@ -6,23 +6,38 @@ function (c, arg) { //start:0, pages:1
         return last_action[0] && last_action[0].t.getTime() == last_action[1].t.getTime();
     }
 
+    function getLevel(level, start) {
+        var a = { start: start };
+        switch(level) {
+            case 4:
+                return #s.scripts.fullsec(a);
+            case 3:
+                return #s.scripts.highsec(a);
+            case 2:
+                return #s.scripts.midsec(a);
+            case 1:
+                return #s.scripts.lowsec(a);
+            case 0:
+                return #s.scripts.nullsec(a);
+            default:
+                throw 'Unknown level';
+        }
+    }
+
     var scripts = [],
         ind = arg.start * 128,
-        t = #s.scripts.fullsec({
-            start: ind
-        });
+        t = getLevel(arg.level, ind);
     while (t.length > 0 && arg.pages > 0) {
         scripts = scripts.concat(t);
         ind = ind + 128;
-        t = #s.scripts.fullsec({
-            start: ind
-        });
+        t = getLevel(arg.level, ind);
         arg.pages--;
     }
 
-    var usrs = scripts.map(x => x.split('.')[0])
+    var usrs = scripts
         .filter(x => {
-            return is_npc(x);
+            var y = x.split('.')[0];
+            return is_npc(y);
         });
 
     return {
